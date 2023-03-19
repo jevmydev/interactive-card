df85d20769769d8bf35a6398772add54c9333e7e
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-19 16:32:37 -0300

    Fix number pattern

--- src/constants.ts
+++ src/constants.ts
@@ -7,5 +7,5 @@ export const INITIAL_CARD_STORE = {
 } as const;
 
 export const STRING_PATTERN = new RegExp("^[a-zA-Z ]*$");
-export const NUMBER_PATTERN = new RegExp("^[0-9]+");
+export const NUMBER_PATTERN = new RegExp("^[0-9]*$");
 export const NUMBER_WITH_SPACES_PATTERN = new RegExp("^[0-9 ]*$");
