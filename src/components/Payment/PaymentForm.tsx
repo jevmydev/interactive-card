bb8ffbf85e6a26ec6adadc017fa4813c80c94b5c
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-19 16:27:20 -0300

    Feat validate errors with react-hook-form

--- src/components/Payment/PaymentForm.tsx
+++ src/components/Payment/PaymentForm.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
-import { INITIAL_CARD_STORE } from "../../constants";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { INITIAL_CARD_STORE, STRING_PATTERN, NUMBER_PATTERN, NUMBER_WITH_SPACES_PATTERN } from "../../constants";
+import { type CardTypes } from "../../types";
 import { useCard } from "../../store/card";
 
 import PaymentSuccess from "./PaymentSuccess";
@@ -7,15 +9,16 @@ import PaymentSuccess from "./PaymentSuccess";
 export const PaymentForm = () => {
     const [isSuccessForm, setIsSuccessForm] = useState<boolean>(false);
     const { name, number, dateMM, dateYY, CVC, updateCard } = useCard();
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+    } = useForm<CardTypes>();
 
     const updateSuccess = () => {
         setIsSuccessForm(!isSuccessForm);
         updateCard(INITIAL_CARD_STORE);
-    };
-
-    const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
-        evt.preventDefault();
-        setIsSuccessForm(!isSuccessForm);
     };
 
     const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,27 +30,147 @@ export const PaymentForm = () => {
         });
     };
 
+    const onSubmit = () => setIsSuccessForm(!isSuccessForm);
+
+    useEffect(() => reset(INITIAL_CARD_STORE), [isSuccessForm]);
+
     return !isSuccessForm ? (
-        <form className="Payment-form" onSubmit={handleSubmit}>
+        <form className="Payment-form" onSubmit={handleSubmit(onSubmit)}>
             <label className="Payment-label">
                 Cardholder name
-                <input className="Payment-input" value={name} onChange={handleChange} name="name" type="text" placeholder="e.g. Jane Appleseed" autoFocus required maxLength={20} />
+                <input
+                    className="Payment-input"
+                    value={name}
+                    type="text"
+                    placeholder="e.g. Jane Appleseed"
+                    autoFocus
+                    maxLength={20}
+                    {...register("name", {
+                        required: "Can't be blank",
+                        maxLength: {
+                            value: 20,
+                            message: "Max length is 20"
+                        },
+                        pattern: {
+                            value: STRING_PATTERN,
+                            message: "String only"
+                        },
+                        onChange: handleChange
+                    })}
+                />
+                {errors.name && <span className="Payment-error">{errors.name.message}</span>}
             </label>
             <label className="Payment-label">
                 Card number
-                <input className="Payment-input" value={number} onChange={handleChange} name="number" type="text" placeholder="e.g. 0000 0000 0000 0000" required minLength={8} maxLength={19} />
+                <input
+                    className="Payment-input"
+                    value={number}
+                    type="text"
+                    placeholder="e.g. 0000 0000 0000 0000"
+                    minLength={8}
+                    maxLength={19}
+                    {...register("number", {
+                        required: "Can't be blank",
+                        minLength: {
+                            value: 8,
+                            message: "Min length is 8"
+                        },
+                        maxLength: {
+                            value: 19,
+                            message: "Max length is 19"
+                        },
+                        pattern: {
+                            value: NUMBER_WITH_SPACES_PATTERN,
+                            message: "Numbers only"
+                        },
+                        onChange: handleChange
+                    })}
+                />
+                {errors.number && <span className="Payment-error">{errors.number.message}</span>}
             </label>
             <div className="Payment-row">
                 <label className="Payment-label">
                     Exp. Date
-                    <div className="Payment-row">
-                        <input className="Payment-input" value={dateMM} onChange={handleChange} name="dateMM" type="text" placeholder="MM" required minLength={2} maxLength={2} />
-                        <input className="Payment-input" value={dateYY} onChange={handleChange} name="dateYY" type="text" placeholder="YY" required minLength={2} maxLength={2} />
-                    </div>
+                    <input
+                        className="Payment-input"
+                        value={dateMM}
+                        type="text"
+                        placeholder="MM"
+                        minLength={2}
+                        maxLength={2}
+                        {...register("dateMM", {
+                            required: "Can't be blank",
+                            minLength: {
+                                value: 2,
+                                message: "Min length is 2"
+                            },
+                            maxLength: {
+                                value: 2,
+                                message: "Max length is 2"
+                            },
+                            pattern: {
+                                value: NUMBER_PATTERN,
+                                message: "Numbers only"
+                            },
+                            onChange: handleChange
+                        })}
+                    />
+                    {errors.dateMM && <span className="Payment-error">{errors.dateMM.message}</span>}
+                </label>
+                <label className="Payment-label">
+                    <input
+                        className="Payment-input"
+                        value={dateYY}
+                        type="text"
+                        placeholder="YY"
+                        minLength={2}
+                        maxLength={2}
+                        {...register("dateYY", {
+                            required: "Can't be blank",
+                            minLength: {
+                                value: 2,
+                                message: "Min length is 2"
+                            },
+                            maxLength: {
+                                value: 2,
+                                message: "Max length is 2"
+                            },
+                            pattern: {
+                                value: NUMBER_PATTERN,
+                                message: "Numbers only"
+                            },
+                            onChange: handleChange
+                        })}
+                    />
+                    {errors.dateYY && <span className="Payment-error">{errors.dateYY.message}</span>}
                 </label>
                 <label className="Payment-label">
                     CVC
-                    <input className="Payment-input" value={CVC} onChange={handleChange} name="CVC" type="text" placeholder="e.g. 123" required minLength={3} maxLength={3} />
+                    <input
+                        className="Payment-input"
+                        value={CVC}
+                        type="text"
+                        placeholder="e.g. 123"
+                        minLength={3}
+                        maxLength={3}
+                        {...register("CVC", {
+                            required: "Can't be blank",
+                            minLength: {
+                                value: 3,
+                                message: "Min length is 3"
+                            },
+                            maxLength: {
+                                value: 3,
+                                message: "Max length is 3"
+                            },
+                            pattern: {
+                                value: NUMBER_PATTERN,
+                                message: "Numbers only"
+                            },
+                            onChange: handleChange
+                        })}
+                    />
+                    {errors.CVC && <span className="Payment-error">{errors.CVC.message}</span>}
                 </label>
             </div>
             <button className="Payment-submit" type="submit">
