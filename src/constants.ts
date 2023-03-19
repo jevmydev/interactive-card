2f6a007033c2e8bc21d1c21905144e59233a88c9
Jeremy Díaz <jeremy664k@gmail.com>
2023-03-19 16:27:09 -0300

    Add pattern validators constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,7 @@ export const INITIAL_CARD_STORE = {
     dateYY: "",
     CVC: ""
 } as const;
+
+export const STRING_PATTERN = new RegExp("^[a-zA-Z ]*$");
+export const NUMBER_PATTERN = new RegExp("^[0-9]+");
+export const NUMBER_WITH_SPACES_PATTERN = new RegExp("^[0-9 ]*$");
